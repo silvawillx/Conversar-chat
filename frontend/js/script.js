cbff13a5c40ade1e4826f6ac42a29a4d13dffb9a
William <emersonwillian2020@gmail.com>
2023-11-17 16:14:44 -0300

    change server url

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -82,7 +82,7 @@ const handleLogin = (event) => {
     login.style.display = "none"
     chat.style.display = "flex"
 
-    websocket = new WebSocket("ws://localhost:8080")
+    websocket = new WebSocket("wss://chat-backend-rrmo.onrender.com")
     websocket.onmessage = processMessage
 }
 
